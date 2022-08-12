a6587cb9eda515e59e56d42bd815e956dd79515d
Romário Ferreira <romarioffranca@gmail.com>
2022-08-12 09:07:12 -0300

    project created

---
+++ src/main/java/com/francaemp/estacionamentodio/EstacionamentoDioApplication.java
@@ -0,0 +1,13 @@
+package com.francaemp.estacionamentodio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacionamentoDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacionamentoDioApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/francaemp/estacionamentodio/EstacionamentoDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francaemp.estacionamentodio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionamentoDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
